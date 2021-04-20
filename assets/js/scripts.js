3c13fde8542c1a05d97714fe48538a3d1618e905
Matty-El <leachmatthewj@gmail.com>
2021-04-20 10:58:53 +0000

    Added additional trail locations, reformatted newsletter modal to align radio buttons, changed navbar poisition, added text to image slider

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,15 @@
-/* --------------------------- TRAILS INFORMATION */
-
 /* --------------------------- GENERAL */
 
 /* Scroll to top on load */
 
-$(document).ready(function(){
-    $(this).scrollTop(0);
+$(document).ready(function() {
+  $(this).scrollTop(0);
 });
 
 /* hero section fade on scroll - https://stackoverflow.com/questions/51118337/how-do-i-make-an-image-fade-out-but-stay-in-place-on-scroll/ */
 
 $(window).scroll(function() {
-  $(".hero-section").css("opacity", 0.65 - $(window).scrollTop() / 500);
+  $("#hero-image").css("opacity", 0.65 - $(window).scrollTop() / 500);
 });
 
 
@@ -19,18 +17,20 @@ $(window).scroll(function() {
 
 /* --------------------------- SLIDES */
 
-$('.slider').each(function() {              // For every slider
-  var $this   = $(this);                    // Current slider
-  var $group  = $this.find('.slide-group'); // Get the slide-group (container)
-  var $slides = $this.find('.slide');       // Create jQuery object to hold all slides
-  var buttonArray  = [];                    // Create array to hold navigation buttons
-  var currentIndex = 0;                     // Hold index number of the current slide
-  var timeout;                              // Sets gap between auto-sliding
+//Sourced from http://www.javascriptbook.com/
 
-  function move(newIndex) {          // Creates the slide from old to new one
-    var animateLeft, slideLeft;      // Declare variables
+$('.slider').each(function() { // For every slider
+  var $this = $(this); // Current slider
+  var $group = $this.find('.slide-group'); // Get the slide-group (container)
+  var $slides = $this.find('.slide'); // Create jQuery object to hold all slides
+  var buttonArray = []; // Create array to hold navigation buttons
+  var currentIndex = 0; // Hold index number of the current slide
+  var timeout; // Sets gap between auto-sliding
 
-    advance();                       // When slide moves, call advance() again
+  function move(newIndex) { // Creates the slide from old to new one
+    var animateLeft, slideLeft; // Declare variables
+
+    advance(); // When slide moves, call advance() again
 
     // If it is the current slide / animating do nothing
     if ($group.is(':animated') || currentIndex === newIndex) {
@@ -38,50 +38,61 @@ $('.slider').each(function() {              // For every slider
     }
 
     buttonArray[currentIndex].removeClass('active'); // Remove class from item
-    buttonArray[newIndex].addClass('active');        // Add class to new item
+    buttonArray[newIndex].addClass('active'); // Add class to new item
 
-    if (newIndex > currentIndex) {   // If new item > current
-      slideLeft = '100%';            // Sit the new slide to the right
-      animateLeft = '-100%';         // Animate the current group to the left
-    } else {                         // Otherwise
-      slideLeft = '-100%';           // Sit the new slide to the left
-      animateLeft = '100%';          // Animate the current group to the right
+    if (newIndex > currentIndex) { // If new item > current
+      slideLeft = '100%'; // Sit the new slide to the right
+      animateLeft = '-100%'; // Animate the current group to the left
+    } else { // Otherwise
+      slideLeft = '-100%'; // Sit the new slide to the left
+      animateLeft = '100%'; // Animate the current group to the right
     }
     // Position new slide to left (if less) or right (if more) of current
-    $slides.eq(newIndex).css( {left: slideLeft, display: 'block'} );
+    $slides.eq(newIndex).css({
+      left: slideLeft,
+      display: 'block'
+    });
 
-    $group.animate( {left: animateLeft}, function() {    // Animate slides and
-      $slides.eq(currentIndex).css( {display: 'none'} ); // Hide previous slide
-      $slides.eq(newIndex).css( {left: 0} ); // Set position of the new item
-      $group.css( {left: 0} );               // Set position of group of slides
-      currentIndex = newIndex;               // Set currentIndex to the new image
+    $group.animate({
+      left: animateLeft
+    }, function() { // Animate slides and
+      $slides.eq(currentIndex).css({
+        display: 'none'
+      }); // Hide previous slide
+      $slides.eq(newIndex).css({
+        left: 0
+      }); // Set position of the new item
+      $group.css({
+        left: 0
+      }); // Set position of group of slides
+      currentIndex = newIndex; // Set currentIndex to the new image
     });
   }
 
-  function advance() {                     // Used to set
-    clearTimeout(timeout);                 // Clear previous timeout
-    timeout = setTimeout(function() {      // Set new timer
+  function advance() { // Used to set
+    clearTimeout(timeout); // Clear previous timeout
+    timeout = setTimeout(function() { // Set new timer
       if (currentIndex < ($slides.length - 1)) { // If slide < total slides
-        move(currentIndex + 1);            // Move to next slide
-      } else {                             // Otherwise
-        move(0);                           // Move to the first slide
+        move(currentIndex + 1); // Move to next slide
+      } else { // Otherwise
+        move(0); // Move to the first slide
       }
-    }, 4000);                              // Milliseconds timer will wait
+    }, 4000); // Milliseconds timer will wait
   }
 
   $.each($slides, function(index) {
     // Create a button element for the button
     var $button = $('<button type="button" class="slide-btn">&bull;</button>');
-    if (index === currentIndex) {    // If index is the current item
-      $button.addClass('active');    // Add the active class
+    if (index === currentIndex) { // If index is the current item
+      $button.addClass('active'); // Add the active class
     }
     $button.on('click', function() { // Create event handler for the button
-      move(index);                   // It calls the move() function
-    }).appendTo('.slide-buttons');   // Add to the buttons holder
-    buttonArray.push($button);       // Add it to the button array
+      move(index); // It calls the move() function
+    }).appendTo('.slide-buttons'); // Add to the buttons holder
+    buttonArray.push($button); // Add it to the button array
   });
 
-  advance();                          // Script is set up, advance() to move it
+  advance(); // Script is set up, advance() to move it
 
 });
 
@@ -89,7 +100,7 @@ $('.slider').each(function() {              // For every slider
 
 function quote() {
 
-  const quotes = [
+  var quotes = [
     "'I’ll be happy if running and I can grow old together' - HARUKI MURAKAMI, WHAT I TALK ABOUT WHEN I TALK ABOUT RUNNING",
     "'Only those who will risk going too far can possibly find out how far they can go.' - T. S. ELIOT",
     "'Ability is what you are capable of doing. Motivation determines what you do. Attitude determines how well you do it.' - LOU HOLTZ",
@@ -99,7 +110,7 @@ function quote() {
   ];
 
   // Display last quote when page loads
-  $("#quote").html(quotes[quotes.length -1]);
+  $("#quote").html(quotes[quotes.length - 1]);
 
   var quotesIndexNo = -1;
 
@@ -109,7 +120,7 @@ function quote() {
       quotesIndexNo = 0;
     }
 
-    $("#quote").fadeOut(2000,function () {
+    $("#quote").fadeOut(2000, function() {
       $("#quote").fadeIn(2000).html(quotes[quotesIndexNo]);
     });
 
@@ -130,11 +141,11 @@ $("document").ready(function() {
 window.onload = function() {
 
   var regions = {
-      'Europe': ['Marathon du Mont Blanc', 'London Marathon', 'Wokingham Half Marathon'],
-      'North America': ['Badwater'],
-      'South America': ['Rio Marathon'],
-      'Africa': ['Marathon des Sables'],
-      'Asia': ['Delhi Marathon']
+    'Europe': ['Marathon du Mont Blanc', 'London Marathon', 'Wokingham Half Marathon'],
+    'North America': ['Badwater'],
+    'South America': ['Rio Marathon'],
+    'Africa': ['Marathon des Sables'],
+    'Asia': ['Delhi Marathon']
   }
 
   var regionSel = document.getElementById("region");
@@ -143,39 +154,32 @@ window.onload = function() {
     regionSel.options[regionSel.options.length] = new Option(x, x);
   }
   regionSel.onchange = function() {
-    //empty Chapters- and Topics- dropdowns
-    raceSel.length = 1;
+    //empty Chapters- and Topics- dropdowns
+    raceSel.length = 1;
     //display correct values
     var z = regions[this.value];
-      for (var i = 0; i < z.length; i++) {
-        raceSel.options[raceSel.options.length] = new Option(z[i], z[i]);
-      }
+    for (var i = 0; i < z.length; i++) {
+      raceSel.options[raceSel.options.length] = new Option(z[i], z[i]);
+    }
   };
-}
+};
 
-function raceSummaryMontBlanc() {
+/* --------------------------- RACE INFORMATION */
 
-$("#race-information").empty().append(
-    `<div>
-            <h2>Marathon du Mont Blanc</h2>
-            <p>Country: France<br>Marathon du Mont Blanc is a well established montain ultra marathon event. A stunning route through the French Alps<br>Difficulty Rating : 10<br>Website: <a href="https://www.marathonmontblanc.fr/en/" target="_blank">Marathon du Mont Blanc</a></p>
-        </div>`
-  );
-}
 
-function raceSummaryBadwater() {
-
-$("#race-information").empty().append(
-    `<div>
-            <h2>Badwater</h2>
-            <p>Country: US<br>Badwater is one of the toughest races in the world.<br>Difficulty Rating : 10<br>Website: <a href="https://www.badwater.com" target="_blank">Badwater</a></p>
-        </div>`
-  );
-}
-
-$("#race").change(function () {
+$("#race").change(function() {
   if ($(this).val() == 'Marathon du Mont Blanc')
-    raceSummaryMontBlanc()
-    else if ($(this).val() == 'Badwater')
-      raceSummaryBadwater()
+    $("#race-information").empty().append(
+      `<div>
+        <h2>Marathon du Mont Blanc</h2>
+        <p>Country: France<br>Marathon du Mont Blanc is a well established montain ultra marathon event. A stunning route through the French Alps<br>Difficulty Rating : 10<br>Website: <a href="https://www.marathonmontblanc.fr/en/" target="_blank">Marathon du Mont Blanc</a></p>
+      </div>`
+    );
+  else if ($(this).val() == 'Badwater')
+    $("#race-information").empty().append(
+      `<div>
+      <h2>Badwater</h2>
+      <p>Country: US<br>Badwater is one of the toughest races in the world.<br>Difficulty Rating : 10<br>Website: <a href="https://www.badwater.com" target="_blank">Badwater</a></p>
+      </div>`
+    );
 });
